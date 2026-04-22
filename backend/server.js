a8f0afb593100136cf656ac7169022c539842ea3
Malik <mohahsanmalik1@gmail.com>
2026-04-22 16:42:56 +0700

    Fix: Handle read-only filesystem on Vercel v1

--- backend/server.js
+++ backend/server.js
@@ -582,7 +582,7 @@ app.post('/api/register', (req, res) => {
 
 // 1. Sajikan folder frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/user/login.html')); // Sesuaikan dengan halaman utama kamu
 });
 
